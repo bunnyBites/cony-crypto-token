f7388c0cb2d0a17b76ae5e4378944d7d8649a654
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-16 13:07:57 +0530

    add authentication in frontend

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,21 @@
-import ReactDOM from 'react-dom'
 import React from 'react'
+import { AuthClient } from "@dfinity/auth-client";
+import ReactDOM from 'react-dom'
 import App from "./components/App";
 
-const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (authClient.isAuthenticated()) {
+    ReactDOM.render(<App />, document.getElementById("root"));
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        ReactDOM.render(<App />, document.getElementById("root"));
+      }
+    })
+  }
 }
 
 init();
